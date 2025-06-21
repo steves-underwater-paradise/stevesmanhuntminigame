31b4d9dfe077d99e323f023e144506d2ab591e1c
Steveplays28 <djspaargaren@outlook.com>
2025-06-21 18:07:26 +0200

    fix: :bug: Fix players spawning in blocks

--- src/main/java/com/steveplays/stevesmanhuntminigame/game/StevesManhuntMiniGameSpawnLogic.java
+++ src/main/java/com/steveplays/stevesmanhuntminigame/game/StevesManhuntMiniGameSpawnLogic.java
@@ -5,8 +5,10 @@ import xyz.nucleoid.plasmid.api.game.GameSpace;
 import xyz.nucleoid.plasmid.api.game.GameSpaceManager;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.GameMode;
+import net.minecraft.world.Heightmap;
 import java.util.Set;
 
 public class StevesManhuntMiniGameSpawnLogic {
@@ -24,15 +26,18 @@ public class StevesManhuntMiniGameSpawnLogic {
         player.changeGameMode(gameMode);
         player.setVelocity(Vec3d.ZERO);
         player.fallDistance = 0f;
+
+        // TODO: Reset player hunger and saturation
+        // TODO: Remove player effects
     }
 
     public void spawnPlayer(ServerPlayerEntity player) {
-        var spawnPosition = this.overworld.getSpawnPos();
-        // TODO: Prevent spawning players in blocks
-        float x = spawnPosition.getX() + MathHelper.nextFloat(player.getRandom(), -this.spawnRadius, this.spawnRadius);
-        float z = spawnPosition.getZ() + MathHelper.nextFloat(player.getRandom(), -this.spawnRadius, this.spawnRadius);
-
-        player.teleport(this.overworld, x, spawnPosition.getY(), z, Set.of(), this.overworld.getSpawnAngle(), 0f);
+        var overworldWorldBorder = this.overworld.getWorldBorder();
+        var playerRandom = player.getRandom();
+        var spawnPosition = BlockPos.ofFloored(overworldWorldBorder.getCenterX() + MathHelper.nextFloat(playerRandom, -this.spawnRadius, this.spawnRadius), 0.0,
+                overworldWorldBorder.getCenterZ() + MathHelper.nextFloat(playerRandom, -this.spawnRadius, this.spawnRadius));
+        spawnPosition = this.overworld.getTopPosition(Heightmap.Type.MOTION_BLOCKING, spawnPosition);
+        player.teleport(this.overworld, spawnPosition.getX(), spawnPosition.getY(), spawnPosition.getZ(), Set.of(), this.overworld.getSpawnAngle(), 0f);
     }
 
     public void respawnPlayer(ServerPlayerEntity player) {
